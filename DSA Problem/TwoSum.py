a9db18e91e584f4466b7f74516639ef2a685c4bb
Ahmad <areza.dev@gmail.com>
2024-07-09 12:02:44 +0530

    deleted dsa folder to structure in better way.

--- DSA Problem/TwoSum.py
+++
@@ -1,9 +0,0 @@
-def two_sum(num_arr, target):
-    for i in range(len(num_arr)):
-        for j in range(i + 1, len(num_arr)):
-            if num_arr[i] + num_arr[j] == target:
-                print(f'Pair of sum ({i} {j})')
-                
-num_arr = [3, 5, 2, -4, 8, 11]
-target = 7
-two_sum(num_arr, target)
